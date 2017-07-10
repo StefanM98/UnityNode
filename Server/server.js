fcda8b8c5f8a012b61e803335cead4106b106b05
StefanM <stefan.fva@gmail.com>
2017-07-09 23:01:49 -0400

    Added server files

---
+++ Server/server.js
@@ -0,0 +1,58 @@
+var io = require('socket.io')(process.env.PORT || 3000);
+var shortid = require('shortid');
+
+console.log('Server Started');
+
+var players = [];
+
+io.on('connection', function(socket){
+  var thisPlayerId = shortid.generate();
+
+  var player = {
+    id: thisPlayerId,
+    x: 0,
+    y:0
+  }
+
+  players[thisPlayerId] = player;
+
+  console.log('Client Connected, broadcasting spawn, id:', thisPlayerId);
+
+  socket.broadcast.emit('spawn', {id: thisPlayerId});
+  socket.broadcast.emit('requestPosition');
+
+  for(var playerId in players) {
+    if(playerId == thisPlayerId)
+      continue;
+
+    socket.emit('spawn', players[playerId]);
+    console.log('sending spawn to new player for id: ', playerId);
+  };
+
+
+  socket.on('updatePosition', function(data) {
+    console.log('Update Position: ', data);
+
+    data.id = thisPlayerId;
+
+    socket.broadcast.emit('updatePosition', data);
+  });
+
+  socket.on('move', function(data) {
+    data.id = thisPlayerId;
+    console.log('client moved', JSON.stringify(data));
+
+    player.x = data.x;
+    player.y = data.y;
+
+    socket.broadcast.emit('move', data)
+  });
+
+  socket.on('disconnect', function(){
+    console.log('client disconnected');
+
+    delete players[thisPlayerId];
+
+    socket.broadcast.emit('disconnected', {id: thisPlayerId});
+  })
+})
